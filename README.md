324a191b935f00f6be682dcaa1e8caf0c329d601
Everton França <everton.r.franca@gmail.com>
2022-03-16 23:11:32 -0300

    creatring structure

--- README.md
+++ README.md
@@ -2,3 +2,56 @@
 
 A profile picture generator for lazy users who don't upload a photo.
 A lightweight and very costumezable javascript profile image generator to execute in browsers.
+Don't spend your time, Optimize it.
+
+## features
+
+- Create your application images, like a boss.
+- Do not spend a lot of time chossing pictures for testing design of your application.
+- If a user don't upload a photo, we create a custom picture to him.
+- Customize your pictures
+
+## Installing
+
+If you just want use for development purposes use the command bellow:\
+\
+`npm i --save-dev profile-awesome`
+
+But, if you want for production purposes, use:\
+\
+`npm i profile-awesome`
+
+## Usage
+
+After install, you need import to your project the core of `profile-awesome`:
+\
+`import ProfileAwesome from "profile-awesome"`\
+\
+now we must define a config object who contains the global rules.
+\
+`const config = {target: "profile-awesome", theme: 'dark', contrast: 5, type: 'auto'};`
+\
+after the above step, just:\
+\
+`ProfileAwesome(config).then(picture => { /* Save on server */ }); /* picture is a object with id of img tag and a base64 encoded png file */`
+\
+Now every `img` tag with `class="profile-awesome"` and `src="#noimage" || src="noimageloader.svg"` will be
+replaced with a beautiful and custom picture.\
+The `src` of a img tag **must** be `#noimage` or `noimageloader.svg` it is for when a user already has a profile
+picture, so we don't replaced it.
+
+## Config Properties
+
+### target
+
+The class of img tag must be searched by profile-awesome.
+
+### theme
+
+the color pallete of pictures, can be assume two different values:
+
+#### dark
+
+basicly the background color with luminosiness and content with more lumosity (just works with a `type: text`)
+
+#### light
--- src/index.js
+++ src/index.js
@@ -13,8 +13,122 @@ console.log("fundo", color1, [r, g, b]);
 
 document.getElementById("color2").style.color = color.hsl2rgb([r, g, b]);
 
-function teste() {
-  return true;
+const colors = {
+  theme: "dark", // or light (dark = color1 dark, color2 light)
+  relation: "complementary", // or Tone,
+  contrast: 5, // min contrast between two colors
+};
+
+function profileAwesome(props) {
+  let _props = {
+    target: "profile-awesome",
+    patternId: "",
+    patternAlt: "",
+    theme: "dark",
+    relation: "complementary",
+    contrast: 5,
+    type: "auto",
+  };
+  for (let key in props) {
+    _props[key] = props[key];
+  }
+  return new Promise((resolve, reject) => {
+    const HTMLCollectionImages = document.getElementsByClassName(_props.target);
+    const images = [];
+    for (let image of HTMLCollectionImages) {
+      images.push({
+        id: image.id.replace(_props.patternId, ""),
+      });
+    }
+    resolve(HTMLCollectionImages);
+  });
 }
 
-export default teste;
+function allProps() {
+  let _props = {
+    target: "profile-awesome",
+    patternId: "",
+    patternAlt: "",
+    theme: "dark",
+    relation: "complementary",
+    contrast: 5,
+    type: "auto",
+  };
+  return _props;
+}
+function noProps(props) {
+  if (props.length === 0) {
+    return allProps();
+  }
+  return false;
+}
+function propsTarget(props) {
+  let _props = allProps();
+  if (props.length === 1 && typeof props[0] === "string") {
+    _props.target = props[0];
+    return _props;
+  }
+  return false;
+}
+
+function propsObject(props) {
+  let _props = allProps();
+  if (props.length === 1 && typeof props[0] === "object") {
+    for (let key in props[0]) {
+      _props[key] = props[0][key];
+    }
+    return _props;
+  }
+  return false;
+}
+
+function propsTargetObject(props) {
+  let _props = allProps();
+  if (
+    props.length === 2 &&
+    typeof props[0] === "string" &&
+    typeof props[1] === "object"
+  ) {
+    for (let key in props[1]) {
+      _props[key] = props[1][key];
+    }
+    _props.target = props[0];
+    return _props;
+  }
+  return false;
+}
+
+function extractProps(props) {
+  if (Array.isArray(props)) {
+    console.error(
+      "Invalid constructor params. Using the default props. Fix it, please."
+    );
+    return allProps();
+  }
+  const _props =
+    noProps(props) ||
+    propsTarget(props) ||
+    propsObject(props) ||
+    propsTargetObject(props);
+
+  if (!_props) {
+    console.error(
+      "Invalid constructor params, using the default props. Fix it, please."
+    );
+    return allProps();
+  }
+  return _props;
+}
+
+function ProfileAwesome(...props) {
+  const _props = extractProps(props);
+  console.log(_props);
+
+  return {
+    run: () => profileAwesome(_props),
+  };
+}
+
+const images = ProfileAwesome(5, 5);
+
+export default ProfileAwesome;
